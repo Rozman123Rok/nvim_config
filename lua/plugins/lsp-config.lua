652982846253a3f396aa4967d0d4bb9b9fc7975b
Rok Rozman <rok.rozman3@gmail.com>
2024-01-27 19:22:39 +0100

    lsp rename keymap

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -33,10 +33,11 @@ return {
 
 			-- Keymaps
 			local opts = {}
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
 		end,
 	},
 }
